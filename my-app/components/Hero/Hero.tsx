f5887d651c0e014273e1368c7f067186733b4be7
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2025-05-29 23:14:48 +0200

    items add

--- my-app/components/Hero/Hero.tsx
+++ my-app/components/Hero/Hero.tsx
@@ -34,6 +34,13 @@ const Hero = () => {
 
   return (
     <section className={styles.hero}>
+
+      <img src="/item1.png" alt="Vector" className={styles.item1}/>
+      <img src="/item2.png" alt="Vector" className={styles.item2}/>
+      <img src="/item3.png" alt="Vector" className={styles.item3}/>
+      <img src="/item4.png" alt="Vector" className={styles.item4}/>
+
+
       <label
         className={styles.toggle}
         style={{ display: "flex", justifyContent: "center", gap: "2%" }}
@@ -65,17 +72,37 @@ const Hero = () => {
         </p>
       </div>
 
-      <div className={styles.heroImage}></div>
+      <div className={styles.heroImageDiv}>
+        <img src="/heroImg2.png" alt="Hero2" className={styles.heroImg1} />
+        <img src="/heroImg1.png" alt="Hero2" className={styles.heroImg2} />
+        <img src="/heroImg3.png" alt="Hero2" className={styles.heroImg3} />
+
+        <span className={styles.tagGreen}>Jetzt verfügbar</span>
+        <span className={styles.blueTag}>reparieren</span>
+        <span className={styles.yellowTag}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="#FFD700"
+          >
+            <path
+              d="M12 .587l3.668 7.431 8.2 1.193-5.934 5.782 
+           1.402 8.178L12 18.896l-7.336 3.855 
+           1.402-8.178L.132 9.211l8.2-1.193z"
+            />
+          </svg>
+
+          4.8
+        </span>
+      </div>
 
       <div className={styles.bottomBtnsWrapper}>
         {dummyBtns.map((btn) => (
           <Button key={btn.id} text={btn.text} style={btn.style} />
         ))}
       </div>
-
-      {/* <form className={styles.bottomForm}>
-        <input type="text" className={styles.bottomInput} placeholder="Search"/>
-      </form> */}
 
       <SearchBar />
     </section>
